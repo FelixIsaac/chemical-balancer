d76d6518d2986a66ffb5fb0f6d6c5c43918a1080
Felix Isaac <limfelix16@gmail.com>
2021-05-13 17:43:55 +0800

    Balancer component

---
+++ app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Balancer/>
+  </div>
+</template>
+
+<script>
+import Balancer from './components/Balancer.vue';
+
+export default {
+  name: 'App',
+  components: { Balancer },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Balancer.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <h1>Chemical Balancer</h1>
+    <el-form inline status-icon @submit.native="submit">
+      <el-form-item>
+        <el-input
+          placeholder="Chemical equation"
+          class="equation"
+          v-model="value"
+          clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="balance">Balance</el-button>
+      </el-form-item>
+    </el-form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Balancer',
+  components: {},
+  data: () => ({
+    isLoading: false,
+    value: '',
+  }),
+  methods: {
+    balance() {
+      console.log(this.value);
+    },
+    submit(e) {
+      e.preventDefault();
+      this.balance();
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 60px;
+  mar-bottom: 10px;
+}
+
+.equation {
+  width: 70vw;
+}
+</style>
